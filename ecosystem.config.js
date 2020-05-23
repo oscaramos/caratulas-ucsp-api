634746d7aa78a50de4a704ac0bb485e15dc2d538
Oscar <oscar.ramos@ucsp.edu.pe>
2020-05-23 14:23:29 -0500

    Added pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps : [{
+    name: "caratulas-ucsp-api",
+    script: "./index.js",
+    instances: 1,
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    }
+  }]
+}
