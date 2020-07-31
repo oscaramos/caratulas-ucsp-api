41a593337e105195d32ee460976bc9df743eed2b
Oscar <oscar.ramos@ucsp.edu.pe>
2020-07-31 15:02:22 -0500

    Refactor - Fill Data Test

--- components/fill-data/fill-data.test.js
+++ components/fill-data/fill-data.test.js
@@ -2,29 +2,27 @@ const fillData = require('./fill-data')
 const dataTest = require('../../TestData')
 
 
-describe('Should complete data when', () => {
+describe('Fill footer data', () => {
+	const expectFooter = (data, footer) => expect(fillData(data).footer).toEqual(footer)
+
 	it('it is male then fill male footer', () => {
-		const data = dataTest.maleStudent
-		const newdata = fillData(data)
-		expect(newdata.footer).toEqual('El alumno declara haber realizado el presente trabajo de acuerdo a las normas de la Universidad Católica San Pablo')
+		expectFooter(dataTest.maleStudent, 'El alumno declara haber realizado el presente trabajo de acuerdo a las normas de la Universidad Católica San Pablo')
 	})
 
 	it('it is female then fill female footer', () => {
-		const data = dataTest.femaleStudent
-		const newdata = fillData(data)
-		expect(newdata.footer).toEqual('La alumna declara haber realizado el presente trabajo de acuerdo a las normas de la Universidad Católica San Pablo')
+		expectFooter(dataTest.femaleStudent, 'La alumna declara haber realizado el presente trabajo de acuerdo a las normas de la Universidad Católica San Pablo')
 	})
 
 	it('it is multiple person then fill multiple footer', () => {
-		const data = dataTest.multipleStudents(2)
-		const newdata = fillData(data)
-		expect(newdata.footer).toEqual('Los alumnos declaran haber realizado el presente trabajo de acuerdo a las normas de la Universidad Católica San Pablo')
+		expectFooter(dataTest.multipleStudents(2), 'Los alumnos declaran haber realizado el presente trabajo de acuerdo a las normas de la Universidad Católica San Pablo')
 	})
+})
+
+describe('Fill names data', () => {
+	const expectNames = (data, names) => expect(fillData(data).names).toEqual(names)
 
 	it('they are multiple person then fill separated names', () => {
-		const data = dataTest.multipleStudents(2)
-		const newdata = fillData(data)
-		expect(newdata.names).toEqual(data.names.join(String.raw` \\ `))
+		expectNames(dataTest.multipleStudents(2), dataTest.multipleStudents(2).names.join(String.raw` \\ `))
 	})
 })
 
