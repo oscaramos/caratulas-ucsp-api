7767967a9719c2b5c2213769bcd9ea4f7f6dc85a
Oscar <oscar.ramos@ucsp.edu.pe>
2020-07-31 14:39:17 -0500

    Change utils file to assets file for conciseness

--- components/rootUtils.utils.js
+++ components/rootUtils.utils.js
@@ -1,6 +1,6 @@
 const path = require('path')
 
-const atUtils = file => path.resolve(path.join(__dirname, '../utils'), file ? file : '')
+const atUtils = file => path.resolve(path.join(__dirname, '../assets'), file ? file : '')
 const atPublic = file => path.resolve(path.join(__dirname, '../public'), file ? file : '')
 
 module.exports = {
